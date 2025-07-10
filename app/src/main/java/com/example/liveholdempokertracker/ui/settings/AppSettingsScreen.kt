55fc974627e075a4e883d7ca2d55f36faaf7d844
Milsil <wdh3436@naver.com>
2025-07-10 16:50:20 +0900

    create-setting
    세팅 관련된 화면 제작 완료.

--- app/src/main/java/com/example/liveholdempokertracker/ui/settings/AppSettingsScreen.kt
+++ app/src/main/java/com/example/liveholdempokertracker/ui/settings/AppSettingsScreen.kt
@@ -1,9 +1,27 @@
 package com.example.liveholdempokertracker.ui.settings
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun AppSettingsScreen() {
-    Text("앱 설정 화면")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text("카드 뒷면 이미지 추가")
+        }
+    }
 }
